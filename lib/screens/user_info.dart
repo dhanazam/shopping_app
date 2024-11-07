37860e84c0c8b09fe25ced5cf2a7e02b7611c7fa
dhanazam <ramadanazam93@gmail.com>
2024-11-08 07:28:17 +0800

    add fab animation

--- lib/screens/user_info.dart
+++ lib/screens/user_info.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/consts/colors.dart';
+import 'package:shopping_app/consts/my_icons.dart';
 
 class UserInfo extends StatefulWidget {
   const UserInfo({super.key});
@@ -10,7 +10,7 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
-  bool _value = false;
+  final bool _value = false;
   late ScrollController _scrollController;
   var top = 0.0;
 
@@ -26,117 +26,248 @@ class _UserInfoState extends State<UserInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        controller: _scrollController,
-        slivers: <Widget>[
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            elevation: 4,
-            expandedHeight: 200,
-            pinned: true,
-            flexibleSpace: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                top = constraints.biggest.height;
-                return Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [
-                          ColorsConsts.starterColor,
-                          ColorsConsts.endColor
-                        ],
-                        begin: const FractionalOffset(0.0, 0.0),
-                        end: const FractionalOffset(1.0, 0.0),
-                        stops: const [0.0, 1.0],
-                        tileMode: TileMode.clamp),
-                  ),
-                  child: FlexibleSpaceBar(
-                    collapseMode: CollapseMode.parallax,
-                    centerTitle: true,
-                    title: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        AnimatedOpacity(
-                          duration: const Duration(milliseconds: 300),
-                          opacity: top <= 110 ? 1.0 : 0.0,
-                          child: Row(
-                            children: [
-                              const SizedBox(
-                                width: 12,
-                              ),
-                              Container(
-                                height: kToolbarHeight / 1.8,
-                                width: kToolbarHeight / 1.8,
-                                decoration: const BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.white,
-                                      blurRadius: 1.0,
-                                    )
-                                  ],
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: NetworkImage(
-                                      'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg',
-                                    ),
+      body: Stack(
+        children: [
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              SliverAppBar(
+                // leading: Icon(Icons.ac_unit_outlined),
+                // automaticallyImplyLeading: false,
+                elevation: 0,
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(builder:
+                    (BuildContext context, BoxConstraints constraints) {
+                  top = constraints.biggest.height;
+
+                  return Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.purple,
+                            Colors.deepPurple,
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: FlexibleSpaceBar(
+                      // collapseMode: CollapseMode.parallax,
+                      centerTitle: true,
+                      title: AnimatedOpacity(
+                        duration: const Duration(milliseconds: 300),
+                        opacity: top <= 110.0 ? 1.0 : 0,
+                        child: Row(
+                          children: [
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            Container(
+                              height: kToolbarHeight / 1.8,
+                              width: kToolbarHeight / 1.8,
+                              decoration: const BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white,
+                                    blurRadius: 1.0,
                                   ),
+                                ],
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(
+                                      'https://t3.ftcdn.net/jpg/01/83/55/76/240_F_183557656_DRcvOesmfDl5BIyhPKrcWANFKy2964i9.jpg'),
                                 ),
                               ),
-                              const SizedBox(
-                                width: 12,
-                              ),
-                              const Text(
-                                'Guest',
-                                style: TextStyle(
-                                    fontSize: 20.0, color: Colors.white),
-                              )
-                            ],
-                          ),
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            const Text(
+                              'Guest',
+                              style: TextStyle(
+                                  fontSize: 20.0, color: Colors.white),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
+                      background: const Image(
+                        image: NetworkImage(
+                            'https://t3.ftcdn.net/jpg/01/83/55/76/240_F_183557656_DRcvOesmfDl5BIyhPKrcWANFKy2964i9.jpg'),
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                    background: const Image(
-                      image: NetworkImage(
-                          'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg'),
-                      fit: BoxFit.fill,
+                  );
+                }),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: userTitle(title: 'User Bag')),
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.grey,
                     ),
-                  ),
-                );
-              },
-            ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.red,
+                        child: const ListTile(
+                          title: Text('Wishlist'),
+                          trailing: Icon(Icons.chevron_right_rounded),
+                          leading: Icon(MyAppIcons.home),
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      onTap: () {},
+                      title: const Text('Cart'),
+                      trailing: const Icon(Icons.chevron_right_rounded),
+                      leading: const Icon(MyAppIcons.cart),
+                    ),
+                    const ListTile(
+                      title: Text('My Orders'),
+                      trailing: Icon(Icons.chevron_right_rounded),
+                      leading: Icon(MyAppIcons.user),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle(title: 'User Information'),
+                    ),
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    userListTile('Email', 'Email here', 0, context),
+                    userListTile('Phone number', '_phoneNumber', 1, context),
+                    userListTile('Shipping address', '', 2, context),
+                    userListTile('joined date', '20.2.2021', 3, context),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle(title: 'User settings'),
+                    ),
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    SwitchListTile(
+                      value: false,
+                      onChanged: (value) {
+                        setState(() {
+                          // themeChange.darkTheme = value;
+                        });
+                      },
+                      visualDensity: VisualDensity.comfortable,
+                      title: const Text('Dark theme'),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        splashColor: Theme.of(context).splashColor,
+                        child: ListTile(
+                          onTap: () async {
+                            // Navigator.canPop(context)? Navigator.pop(context):null;
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext ctx) {
+                                  return AlertDialog(
+                                    title: Row(
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 6.0),
+                                          child: Image.network(
+                                            'https://image.flaticon.com/icons/png/128/1828/1828304.png',
+                                            height: 20,
+                                            width: 20,
+                                          ),
+                                        ),
+                                        const Padding(
+                                          padding: EdgeInsets.all(8.0),
+                                          child: Text('Sign out'),
+                                        ),
+                                      ],
+                                    ),
+                                    content:
+                                        const Text('Do you wanna Sign out?'),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () async {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text('Cancel')),
+                                      TextButton(
+                                          onPressed: () async {},
+                                          child: const Text(
+                                            'Ok',
+                                            style: TextStyle(color: Colors.red),
+                                          ))
+                                    ],
+                                  );
+                                });
+                          },
+                          title: const Text('Logout'),
+                          leading: const Icon(Icons.exit_to_app_rounded),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          SliverToBoxAdapter(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: userTitle('Account Information'),
-                ),
-                const Divider(
-                  thickness: 1,
-                  color: Colors.grey,
-                ),
-                userListTile('Email', 'Email sub', Icons.email, context),
-                userListTile('Email', 'Email sub', Icons.email, context),
-                userListTile('Email', 'Email sub', Icons.email, context),
-                userListTile('Email', 'Email sub', Icons.email, context),
-                userListTile('Email', 'Email sub', Icons.email, context),
-                const Padding(padding: EdgeInsets.only(left: 8.0)),
-                SwitchListTile(
-                  value: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
-                ),
-                userListTile('Email', 'Email sub', Icons.email, context),
-              ],
-            ),
-          )
+          _buildFab()
         ],
+      ),
+    );
+  }
+
+  Widget _buildFab() {
+    //starting fab position
+    const double defaultTopMargin = 200.0 - 4.0;
+    //pixels from top where scaling should start
+    const double scaleStart = 160.0;
+    //pixels from top where scaling should end
+    const double scaleEnd = scaleStart / 2;
+
+    double top = defaultTopMargin;
+    double scale = 1.0;
+    if (_scrollController.hasClients) {
+      double offset = _scrollController.offset;
+      top -= offset;
+      if (offset < defaultTopMargin - scaleStart) {
+        //offset small => don't scale down
+
+        scale = 1.0;
+      } else if (offset < defaultTopMargin - scaleEnd) {
+        //offset between scaleStart and scaleEnd => scale down
+
+        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+      } else {
+        //offset passed scaleEnd => hide fab
+        scale = 0.0;
+      }
+    }
+
+    return Positioned(
+      top: top,
+      right: 16.0,
+      child: Transform(
+        transform: Matrix4.identity()..scale(scale),
+        alignment: Alignment.center,
+        child: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          heroTag: "btn1",
+          onPressed: () {},
+          child: const Icon(Icons.camera_alt_outlined),
+        ),
       ),
     );
   }
@@ -151,23 +282,24 @@ class _UserInfoState extends State<UserInfo> {
     );
   }
 
+  final List<IconData> _userTileIcons = [
+    Icons.email,
+    Icons.phone,
+    Icons.local_shipping,
+    Icons.watch_later,
+    Icons.exit_to_app_rounded
+  ];
+
   Widget userListTile(
-      String title, String subtitle, IconData icon, BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        splashColor: Theme.of(context).splashColor,
-        child: ListTile(
-          onTap: () {},
-          title: Text(title),
-          subtitle: Text(subtitle),
-          leading: Icon(icon),
-        ),
-      ),
+      String title, String subTitle, int index, BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      leading: Icon(_userTileIcons[index]),
     );
   }
 
-  Widget userTitle(String title) {
+  Widget userTitle({required String title}) {
     return Padding(
       padding: const EdgeInsets.all(14.0),
       child: Text(
